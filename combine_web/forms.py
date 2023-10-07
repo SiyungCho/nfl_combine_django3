a30dc3688d06575d9f978fd51332f0339dad72b8
Siyung Cho <20fsc@queensu.ca>
2023-10-07 18:19:26 -0400

    work on input form

--- combine_web/forms.py
+++ combine_web/forms.py
@@ -1,7 +1,7 @@
 from django.forms import ModelForm
 from .models import input_data
 
-class ToDo_Form(ModelForm):
+class input_data_Form(ModelForm):
     class Meta:
         model = input_data
         fields = ['name',"player_POS", "player_Height", "player_Weight", "player_40Yard", "player_BenchPress", "player_Vert", "player_Broad", "player_Shuttle", "player_3Cone"]
--- combine_web/views.py
+++ combine_web/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .forms import input_data_Form
+from .models import input_data
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
+def home(request):
+    return render(request, 'combine_web/base.html')
+
+def input(request):
+    return render(request, 'combine_web/input.html', {'form':UserCreationForm()})
