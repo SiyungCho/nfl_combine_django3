68352e3c8ae8fc7d1d65bf1bd4b3df9375b7134f
Siyung Cho <20fsc@queensu.ca>
2023-10-09 03:44:49 -0400

    add postgres

--- nfl_combine_web/settings.py
+++ nfl_combine_web/settings.py
@@ -135,4 +135,8 @@ WHITENOISE_KEEP_ONLY_HASHED_FILES = True
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+if 'DATABASE_URL' in os.environ:
+    import dj_database_url
+    DATABASES = {'default': dj_database_url.config()}
+
 django_heroku.settings(locals())
